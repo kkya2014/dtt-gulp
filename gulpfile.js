b1c04a960c1016939b6795bb2403a8113143b7e3
kkya <kkya2014@163.com>
2015-12-28 11:03:11 +0800

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,6 @@ var gulp = require('gulp'), //基础库
     livereload = require('gulp-livereload'), //livereload
     paths = {
         root: './',
-        bak: 'bak/',
         dist: {
             root: 'examples/dist/',
             styles: 'examples/dist/css/',
@@ -24,17 +23,13 @@ var gulp = require('gulp'), //基础库
         co: {
             root: 'co/',
             styles: 'co/css/',
-            scripts: 'co/',
-            libs: 'co/libs/',
-            ui: 'co/ui/'
+            scripts: 'co/'
         },
         source: {
             root: 'src/co-modules/',
             styles: 'src/co-modules/less/',
             scripts: 'src/co-modules/js/',
-            libs: 'src/co-modules/js/libs/',
-            ui: 'src/co-modules/js/ui/',
-            examples: 'src/examples/'
+            libs: 'libs/'
         },
         examples: {
             root: 'examples/',
@@ -44,51 +39,45 @@ var gulp = require('gulp'), //基础库
     co = {
         filename: 'co',
         jsFiles: [
-            'src/co-modules/js/base/sea.js',
-            'src/co-modules/js/base/config.js',
-            'src/co-modules/js/co.js'
-        ]
-    },
-    dom = {
-        filename: 'dom',
-        jsFiles: [
-            'src/co-modules/js/base/dom/dom.js',
-            'src/co-modules/js/base/dom/plugins/event.js',
-            'src/co-modules/js/base/dom/plugins/ajax.js',
-            'src/co-modules/js/base/dom/plugins/fx.js',
-            'src/co-modules/js/base/dom/plugins/fx_methods.js',
-            'src/co-modules/js/base/dom/plugins/data.js',
-            'src/co-modules/js/base/dom/plugins/highlight.js',
-            'src/co-modules/js/base/dom/plugins/detect.js',
-            'src/co-modules/js/base/dom/plugins/touch.js',
-            'src/co-modules/js/base/dom/plugins/matchMedia.js',
+            'libs/iscroll.js',
+            'src/co-modules/js/co.js',
+            'src/co-modules/js/zepto.extend.js',
             'src/co-modules/js/$extend.js',
-            'src/co-modules/js/base/dom/plugins/ex-ortchange.js',
-            'src/co-modules/js/$fn_extend.js'
-            // 'src/co-modules/js/base/zepto/plugins/deferred.js',
-            // 'src/co-modules/js/base/zepto/plugins/callbacks.js',
-            // 'src/co-modules/js/base/zepto/plugins/selector.js',
-            // 'src/co-modules/js/base/zepto/plugins/stack.js',
-            // 'src/co-modules/js/base/zepto/plugins/form.js',
+            'src/co-modules/js/widgets/slider/slider.js',
+            'src/co-modules/js/widgets/slider/touch.js',
+            'src/co-modules/js/widgets/slider/guide.js',
+            'src/co-modules/js/widgets/slider/multiview.js',
+            'src/co-modules/js/widgets/slider/gestures.js',
+            'src/co-modules/js/widgets/accordion.js',
+            'src/co-modules/js/widgets/accordionList.js',
+            'src/co-modules/js/widgets/fullpage.js',
+            'src/co-modules/js/widgets/input.js',
+            'src/co-modules/js/widgets/lazyloadimage.js',
+            'src/co-modules/js/widgets/navigator.js',
+            'src/co-modules/js/widgets/photobrowser.js',
+            'src/co-modules/js/widgets/refresh.js',
+            'src/co-modules/js/widgets/searchbar.js',
+            'src/co-modules/js/widgets/swipelist.js',
+            'src/co-modules/js/widgets/swipepage.js',
+            'src/co-modules/js/widgets/switch.js',
+            'src/co-modules/js/widgets/tabs.js'
+            
         ]
     },
     zepto = {
         filename: 'zepto',
         jsFiles: [
-            'src/co-modules/js/base/zepto/zepto.js',
-            'src/co-modules/js/base/zepto/zepto.extend.js',
-            'src/co-modules/js/base/zepto/plugins/event.js',
-            'src/co-modules/js/base/zepto/plugins/ajax.js',
-            'src/co-modules/js/base/zepto/plugins/fx.js',
-            'src/co-modules/js/base/zepto/plugins/fx_methods.js',
-            'src/co-modules/js/base/zepto/plugins/data.js',
-            'src/co-modules/js/base/zepto/plugins/highlight.js',
-            'src/co-modules/js/base/zepto/plugins/detect.js',
-            'src/co-modules/js/base/zepto/plugins/touch.js',
-            'src/co-modules/js/base/zepto/plugins/matchMedia.js',
-            'src/co-modules/js/$extend.js',
-            'src/co-modules/js/base/zepto/plugins/ex-ortchange.js',
-            'src/co-modules/js/$fn_extend.js'
+            'zepto/zepto.js',
+            'zepto/plugins/event.js',
+            'zepto/plugins/ajax.js',
+            'zepto/plugins/fx.js',
+            'zepto/plugins/fx_methods.js',
+            'zepto/plugins/data.js',
+            'zepto/plugins/highlight.js',
+            'zepto/plugins/detect.js',
+            'zepto/plugins/touch.js',
+            'zepto/plugins/matchMedia.js',
+            'zepto/plugins/ex-ortchange.js',
         ]
     },
     banner = {
@@ -129,28 +118,7 @@ gulp.task('co-scripts', function(cb) {
         .pipe(header(banner.header, {
             date: date
         }))
-        .pipe(gulp.dest(paths.bak + date.year + date.month + date.day))
-        // .pipe(rename({
-        //     suffix: '.min'
-        // }))
-        .pipe(uglify())
-        .pipe(header(banner.header, {
-            date: date
-        }))
         .pipe(gulp.dest(paths.co.scripts))
-        .on('finish', function() {
-            cb();
-        });
-});
-
-//dom处理
-gulp.task('co-dom', function(cb) {
-    gulp.src(dom.jsFiles) //要合并的文件
-        .pipe(concat(dom.filename + ".js")) // 合并匹配到的js文件并命名为 "all.js"
-        .pipe(header(banner.header, {
-            date: date
-        }))
-        .pipe(gulp.dest(paths.bak + date.year + date.month + date.day))
         // .pipe(rename({
         //     suffix: '.min'
         // }))
@@ -171,7 +139,7 @@ gulp.task('co-zepto', function(cb) {
         .pipe(header(banner.header, {
             date: date
         }))
-        .pipe(gulp.dest(paths.bak + date.year + date.month + date.day))
+        .pipe(gulp.dest(paths.co.scripts))
         // .pipe(rename({
         //     suffix: '.min'
         // }))
@@ -185,30 +153,11 @@ gulp.task('co-zepto', function(cb) {
         });
 });
 
-//libs处理
-gulp.task('co-libs', function(cb) {
-    gulp.src(paths.source.libs + '*')
-        .pipe(gulp.dest(paths.co.libs))
-        .on('finish', function() {
-            cb();
-        });
-});
-
-
-//ui处理
-gulp.task('co-ui', function(cb) {
-    gulp.src(paths.source.ui + '**/*.*')
-        .pipe(gulp.dest(paths.co.ui))
-        .on('finish', function() {
-            cb();
-        });
-});
-
 // co样式处理
 gulp.task('co-css', function(cb) {
     gulp.src('src/co-modules/less/co.less')
         .pipe(less())
-        .pipe(gulp.dest(paths.bak + date.year + date.month + date.day))
+        .pipe(gulp.dest(paths.co.styles))
         .pipe(minifycss({
             advanced: false,
             aggressiveMerging: false,
@@ -222,15 +171,6 @@ gulp.task('co-css', function(cb) {
         });
 });
 
-// //co图片处理
-// gulp.task('co-img', function(cb) {
-//     gulp.src(paths.source.root + 'img/*.*')
-//         .pipe(gulp.dest(paths.co.root + 'img/'))
-//         .on('finish', function() {
-//             cb();
-//         });
-// });
-
 //co字体处理
 gulp.task('co-font', function(cb) {
     gulp.src(paths.source.root + 'fonts/*.*')
@@ -241,7 +181,7 @@ gulp.task('co-font', function(cb) {
 });
 
 //co处理
-gulp.task('build-co', gulp.series('cleanCo', 'co-scripts', 'co-dom', 'co-zepto', 'co-libs', 'co-ui', 'co-css', 'co-font'));
+gulp.task('build-co', gulp.series('cleanCo', 'co-scripts', 'co-zepto', 'co-css', 'co-font'));
 
 // 清空dist样式
 gulp.task('cleanDist', function(cb) {
@@ -273,16 +213,7 @@ gulp.task('dist-css', function(cb) {
         });
 });
 
-// //dist图片处理
-// gulp.task('dist-img', function(cb) {
-//     gulp.src(paths.source.root + 'img/*.*')
-//         .pipe(gulp.dest(paths.dist.root + 'img/'))
-//         .on('finish', function() {
-//             cb();
-//         });
-// });
-
-//dist字体处理错误: 没有找到进程 "node.exe"。
+//dist字体
 gulp.task('dist-font', function(cb) {
     gulp.src(paths.source.root + 'fonts/*.*')
         .pipe(gulp.dest(paths.dist.root + 'fonts/'))
@@ -294,24 +225,6 @@ gulp.task('dist-font', function(cb) {
 // 样式处理
 gulp.task('dist-styles', gulp.series('cleanDist', 'dist-css', 'dist-font'));
 
-
-//libs处理
-gulp.task('dist-libs', function(cb) {
-    gulp.src(paths.source.libs + '*')
-        .pipe(gulp.dest(paths.dist.libs))
-        .on('end', function() {
-            cb();
-        });
-});
-
-// ui处理
-gulp.task('dist-ui', function(cb) {
-    gulp.src(paths.source.ui + '**/*.*')
-        .pipe(gulp.dest(paths.dist.ui))
-        .on('end', function() {
-            cb();
-        });
-});
 
 // js处理
 gulp.task('dist-scripts', function(cb) {
@@ -330,8 +243,8 @@ gulp.task('dist-scripts', function(cb) {
 });
 
 // dom处理
-gulp.task('dist-dom', function(cb) {
-    gulp.src(dom.jsFiles) //要合并的文件
+gulp.task('dist-zepto', function(cb) {
+    gulp.src(zepto.jsFiles) //要合并的文件
         .pipe(concat(zepto.filename + ".js")) // 合并匹配到的js文件并命名为 "all.js"
         .pipe(gulp.dest(paths.dist.scripts))
         // .pipe(rename({
@@ -345,25 +258,9 @@ gulp.task('dist-dom', function(cb) {
         });
 });
 
-// dom处理
-gulp.task('dist-zepto', function(cb) {
-    gulp.src(zepto.jsFiles) //要合并的文件
-        .pipe(concat(dom.filename + ".js")) // 合并匹配到的js文件并命名为 "all.js"
-        .pipe(gulp.dest(paths.dist.scripts))
-        // .pipe(rename({
-        //     suffix: '.min'
-        // }))
-        // .pipe(uglify())
-        .pipe(gulp.dest(paths.dist.scripts))
-        .pipe(livereload())
-        .on('end', function() {
-            cb();
-        });
-});
 
 
-
-gulp.task('dist-js', gulp.series('dist-libs', 'dist-ui', 'dist-scripts', 'dist-dom', 'dist-zepto'));
+gulp.task('dist-js', gulp.series('dist-scripts', 'dist-zepto'));
 
 gulp.task('build-dist', gulp.series('dist-styles', 'dist-js'));
 
--- src/co-modules/js/co.js
+++ src/co-modules/js/co.js
@@ -2,30 +2,19 @@
 	/*===============================================================================
 	************   co start   ************
 	===============================================================================*/
-;
-(function(global, loader, undefined) {
+;(function(global, $, undefined) {
 	var co = global.co = {
-		// The current version of co.js being used
-		version: "1.0.1",
-		verticalSwipe: true //是否可以纵向滑动
-	}
+			// The current version of co.js being used
+			version: "1.0.1",
+			verticalSwipe: true //是否可以纵向滑动
+		},
+		$ui = {},
+		Base = {};
 	var readyRE = /complete|loaded|interactive/;
 
 	var REQUIRE_RE = /"(?:\\"|[^"])*"|'(?:\\'|[^'])*'|\/\*[\S\s]*?\*\/|\/(?:\\\/|[^\/\r\n])+\/(?=[^\/])|\/\/.*|\.\s*require|(?:^|[^$])\brequire\s*\(\s*(["'])(.+?)\1\s*\)/g
 	var SLASH_RE = /\\\\/g
 
-	function parseDependencies(code) {
-		var ret = []
-
-		code.replace(SLASH_RE, "")
-			.replace(REQUIRE_RE, function(m, m1, m2) {
-				if (m2) {
-					ret.push(m2)
-				}
-			})
-
-		return ret
-	}
 	if (($.os.android || $.os.ios)) {
 		// if (($.os.ios) && rd.app.isFullScreen()) {
 		if ($.os.ios) {
@@ -33,27 +22,194 @@
 		}
 	}
 
+	var getWidget = function(name) {
+		return $ui.widgets[name]
+	};
+
+	Base.eachObj = function(obj, iterator) {
+		obj && Object.keys(obj).forEach(function(key) {
+			iterator(key, obj[key]);
+		});
+	};
+	Base.getWidget = getWidget;
+	Base.register = function(name, callback) {
+		if ($.isFunction(callback)) {
+			callback.call(global, $ui.plugins[name])
+		}
+	};
+	Base.init = function() {};
+	/**
+	 * @name extend
+	 * @desc 扩充现有组件
+	 */
+	Base.extend = function(obj) {
+		var proto = this.prototype;
+		Base.eachObj(obj, function(key, val) {
+			proto[key] = val;
+		});
+		return this;
+	};
+
+	Base.parseTpl = function(str, data) {
+		var tmpl = 'var __p=[];' + 'with(obj||{}){__p.push(\'' +
+			str.replace(/\\/g, '\\\\')
+			.replace(/'/g, '\\\'')
+			.replace(/<%=([\s\S]+?)%>/g, function(match, code) {
+				return '\',' + code.replace(/\\'/, '\'') + ',\'';
+			})
+			.replace(/<%([\s\S]+?)%>/g, function(match, code) {
+				return '\');' + code.replace(/\\'/, '\'')
+					.replace(/[\r\n\t]/g, ' ') + '__p.push(\'';
+			})
+			.replace(/\r/g, '\\r')
+			.replace(/\n/g, '\\n')
+			.replace(/\t/g, '\\t') +
+			'\');}return __p.join("");',
+
+			func = new Function('obj', tmpl);
+
+		return data ? func(data) : func;
+	};
+
+	/*
+	      判断是否Touch屏幕
+	  */
+	Base.isTouchScreen = function() {
+		return (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch);
+	};
+
+	Base.touchEve = function() {
+		return this.isTouchScreen() ? "tap" : "click"
+	};
+
+	Base.touchStart = function() {
+		return this.isTouchScreen() ? "touchstart" : "mousedown"
+	};
+
+	Base.touchEnd = function() {
+		return this.isTouchScreen() ? "touchend" : "mouseup"
+	};
+
+	Base.touchCancel = function() {
+		return this.isTouchScreen() ? "touchcancel" : "mouseup"
+	};
+
+	Base.touchMove = function() {
+		return this.isTouchScreen() ? "touchmove" : "mouseup"
+	};
+
+	Base.longTap = function() {
+		return this.isTouchScreen() ? "longTap" : "mouseup"
+	};
+
+	Base.touchOver = function() {
+		return this.isTouchScreen() ? "touchend touchmove" : "mouseup"
+	};
+
+
+	Base.log = function(str) {
+		console.log(str);
+		return this;
+	};
+	Base.callZ = (function() {
+		instance = $();
+		instance.length = 1;
+
+		return function(item) {
+			instance[0] = item;
+			return instance;
+		};
+	})()
+
+	Base.stopPropagation = function(e) {
+		e.stopPropagation();
+		return this;
+	};
+
+	Base.preventDefault = function(e) {
+		e.preventDefault();
+		return this;
+	};
+
+
+	Base.focus = function(element) {
+		if ($.os.ios) {
+			setTimeout(function() {
+				element.focus();
+			}, 10);
+		} else {
+			element.focus();
+		}
+		return this;
+	};
+
+	$ui.uuid = 0;
+	$ui.data = {};
+	$ui.widgets = {};
+	$ui.plugins = {};
+	$ui.module = {};
+	$ui.define = function(name, options) {
+		if ($ui.widgets[name]) return $ui.widgets[name];
+		var defOpts = {
+			/**
+			 * 参照对象
+			 * @property {String} [ref=null]
+			 */
+			ref: null, //参照目标 
+
+			/**
+			 * 点击回调函数
+			 * @type {function}
+			 */
+			callback: null
+		}
+		var klass = function(opts) {
+			var baseOpts = $.extend(true, {}, this.options);
+			this.opts = $.extend(true, baseOpts, opts);
+			this.ref = $(this.opts.ref);
+			this.callback = this.opts.callback;
+			this.$family = {
+				name: name
+			}
+			this.init();
+		}
+		$ui.widgets[name] = Base.extend.call(klass, Base);
+		$ui.widgets[name].prototype.options = $.extend(defOpts, options);
+		return $ui.widgets[name];
+	};
+
+	$ui.plugin = function(name, factory) {
+		$ui.plugins[name] = factory
+	};
+
+	var define = function(factory) {
+		if ($.isFunction(factory)) {
+			var module = factory.call(global, $ui)
+		}
+	};
+
+	var require = function(widget) {
+		var widget = getWidget(widget);
+		return widget;
+	}
 
 	var domReady = function(factory) {
 		if ($.isFunction(factory)) {
-			var deps = parseDependencies(factory.toString())
-			
-			loader.use(deps, function() {
-				if (($.os.android || $.os.ios) && co.plus) {
-					setTimeout(function() {
-						if (domReady.isReady) {
-							factory.call(null, loader.require);
-						} else {
-							setTimeout(arguments.callee, 1);
-						}
-					}, 1);
-				} else {
-					factory.call(null, loader.require);
-				}
 
-				$(document).find('.ui-action-back').button(function(evt) {
-					window.history.back()
-				})
+			if (($.os.android || $.os.ios) && co.plus) {
+				setTimeout(function() {
+					if (domReady.isReady) {
+						factory.call(global,require);
+					} else {
+						setTimeout(arguments.callee, 1);
+					}
+				}, 1);
+			} else {
+				factory.call(global,require);
+			}
+
+			$(document).find('.ui-action-back').button(function(evt) {
+				window.history.back()
 			})
 		}
 	};
@@ -73,4 +229,5 @@
 	};
 
 	global.domReady = domReady;
-})(this, seajs);
+	global.define = define;
+})(this, $);
